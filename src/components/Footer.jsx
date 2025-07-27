bd0257a0f9116841835b7c14020e16416a9203af
Ruchira Sonar <ruchirars05@gmail.com>
2025-07-27 22:46:08 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer-content">
         <p>© {new Date().getFullYear()} Ruchira RS. All rights reserved.</p>
         <div className="footer-icons">
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="footer-icon"><FaGithub /></a>
+          <a href="https://github.com/RuchiraRS" target="_blank" rel="noopener noreferrer" className="footer-icon"><FaGithub /></a>
           <a href="mailto:ruchirars05@gmail.com" className="footer-icon"><MdEmail /></a>
           <a href="https://www.linkedin.com/in/ruchira-rs-6a1807343/" target="_blank" rel="noopener noreferrer" className="footer-icon"><FaLinkedin /></a>
           <a href="#home" className="footer-icon top-link"><FaArrowUp /></a>
